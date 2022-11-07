b6c78ea2de52c567f583c4e146e21b16896031be
BenjaminHuang <peternick0207@gmail.com>
2022-11-06 16:31:53 -0800

    deleteGrids-function-update

--- source/calendar/calendar-js.js
+++ source/calendar/calendar-js.js
@@ -2,19 +2,27 @@ window.addEventListener('DOMContentLoaded', init);
 
 
 function getDaysInMonth(year, month) {
+    // month starts at 0 index
+    //passing in 0 to get the previous month
+    //ex: (2022, 07, 0) => # of days in June.
     return new Date(year, month, 0).getDate();
   }
 
-function createGrids(){
+function getFirstDay(year, month) {
+    return new Date(year, month - 1, 1).getDay();
+}
 
+
+function createGrids(year, month){
 
     const today = new Date();
-    const firstDay = new Date(today.getFullYear(), today.getMonth(), 1).getDay();
-    const numberDays = getDaysInMonth(today.getFullYear(), today.getMonth());
-    console.log(numberDays)
-    //Tues, Monday...
-    const target = document.querySelector('.date-grid')
+    const firstDay = getFirstDay(year, month);
+    const numberDays = getDaysInMonth(year, month);
+    console.log(numberDays);
+    console.log(firstDay);
 
+    const target = document.querySelector('.date-grid')
+    
     //grids before first day of the month
     for(let i = 0; i < firstDay; i++){
         const grid = document.createElement('div');
@@ -22,11 +30,11 @@ function createGrids(){
     }
 
     //grids in the month
-    for(let i = 1; i < numberDays; i++){
+    for(let i = 1; i <= numberDays; i++){
         const grid = document.createElement('div');
         target.appendChild(grid);
         
-        if(today.getDate() == i){
+        if(today.getDate() == i && today.getMonth() + 1 == month){
             grid.className = 'day today';
         }
         else
@@ -34,15 +42,29 @@ function createGrids(){
     }
 }
 
+//delete the whole grids for refreshing purpose.
+function deleteGrids(){
+    const target = document.querySelector('.date-grid');
+    const child = target.querySelectorAll('div')
+    for(let i = 0; i < child.length; i++){
+        child[i].remove();
+    }
+}
+
 function init(){
 
-    const date = new Date();
+    const date = new Date(); //get today's date
     const year = date.getFullYear(); //current year
     const month = date.getMonth() + 1; //current month
-
-    const daysInCurrentMonth = getDaysInMonth(year, month); // # of days in current month
-
-    console.log(daysInCurrentMonth);
+    createGrids(year, month);
     
-    createGrids();
+    const test = document.querySelector('#start');
+
+    test.addEventListener('change', ()=>{
+        deleteGrids(); //refresh the calender before generating.
+        const st = test.value.split("-");   
+        console.log(st[0] - 0, st[1] - 0);
+        createGrids(st[0] - 0, st[1] - 0);
+    });
+
 }
