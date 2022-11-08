ad1e03d6f53208a6f42f1e05b18b8123ddd051ab
Tianyu Chen <tianyuchen1016@gmail.com>
2022-11-08 15:12:31 -0800

    add prompt page html and js for form data storage

---
+++ source/hello-page/hello-event.js
@@ -0,0 +1,46 @@
+// when clicking finish, store the inputs into localStorage as an entry for the day
+
+let dateObj = new Date();
+let date = dateObj.getDate();
+let month = dateObj.getMonth() + 1;
+let year = dateObj.getFullYear();
+let key = `${year}-${month}-${date}`;
+
+let form = document.querySelector("form");
+if (form != null) {
+    form.addEventListener("submit", submitAndStore);
+}
+
+let goButton = document.querySelector(".go-button");
+if (goButton != null) {
+    goButton.addEventListener("click", checkAndGo);
+}
+
+/**
+ * handle the prompt page form submission and store the information 
+ * as an entry with date as key in localStorage
+ */
+function submitAndStore() {
+    let formData = new FormData(form);
+    let feeling = formData.get("feelings");
+    let comment = formData.get("comment");
+
+    let entry = new Object();
+    entry.rating = feeling;
+    entry.comment = comment;
+    entry.done = true;
+    entry.edited = false;
+
+    localStorage.setItem(key, JSON.stringify(entry));
+
+    location.href = "finish-page.html";
+}
+
+function checkAndGo() {
+    if (localStorage.getItem(key) == null) {
+        location.href = "prompt-page.html";
+    }
+    else {
+        location.href = "finish-page.html";
+    }
+}
