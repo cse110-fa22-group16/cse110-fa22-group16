df8ef34950623efdbe9f488233670727b06f4afb
Tianyu Chen <tianyuchen1016@gmail.com>
2022-11-22 13:57:38 -0800

    set currentState to null on clicking navbar

--- source/hello-page/add-navbar.js
+++ source/hello-page/add-navbar.js
@@ -1,8 +1,8 @@
 let header = document.querySelector("header");
 header.innerHTML = `
-<a href="home-page.html"><img src="image/ref.png" alt="Reflect-logo"></a>
+<a href="home-page.html" onclick="resetState()"><img src="image/ref.png" alt="Reflect-logo"></a>
 <span class="date-today"></span>
-<a href="../calendar/index.html" class="to-calendar">my calendar</a>`;
+<a href="../calendar/index.html" class="to-calendar" onclick="resetState()">my calendar</a>`;
 
 /* for dynamic date */
 let dateObjDynamic = new Date();
@@ -13,4 +13,9 @@ let yearDynamic = dateObjDynamic.getFullYear();
 let headerDate = document.querySelector('header>.date-today');
 if (headerDate != null) {
     headerDate.textContent = `${monthDynamic}/${dateDynamic}/${yearDynamic}`;
+}
+
+//For navbar clicks
+function resetState(){
+    window.localStorage.setItem('currentState', null);
 }
