299b7af726964ee4cfd5ed8d76ace8d513670ee2
hanghanghappy <chc063@ucsd.edu>
2022-12-01 19:54:47 -0800

    Fixed edit_test.js

--- source/edit-page/__tests__/edit_test.js
+++ source/edit-page/__tests__/edit_test.js
@@ -5,12 +5,14 @@ let year = dateObj.getFullYear();
 let key = `${year}-${month}-${date}`;
 const moodList = ['terrible', 'bad', 'neutral', 'good', 'great'];
 
+jest.setTimeout(100000);
 // I was not able to directly access edit page so I had to click through the other pages to access it.
+
 
 describe('Test edit page from finish page', () => {
     beforeAll(async () => {
         await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/finish-page.html');
-    }, 100000);    
+    });    
     it('Check the entry button link to edit page', async () => {
         await page.click('button');
         const title = await page.title();
@@ -21,9 +23,10 @@ describe('Test edit page from finish page', () => {
 describe('Test edit page', () => {
     beforeAll(async() => {
         await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/prompt-page.html');
-    }, 100000);
+    });
 
     for(let i = 0; i < 5; i++) {
+
         it(`Check edit page for the following mood: (${moodList[i]})`, async () => {
             await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/prompt-page.html');
             const navigationPromise = page.waitForNavigation({ waitUntil: ['load', 'networkidle2'] });
@@ -38,22 +41,26 @@ describe('Test edit page', () => {
             const titleEdit = await page.title();
             expect(titleEdit).toBe('Edit Page');
 
+
             // Text of the Edit Button
             const editButton = await page.$('#editButton');
-            const value = await editButton.getProperty('value');
+            const value = await editButton.getProperty('innerText');
             const rawValue = await value.jsonValue();
             expect(rawValue).toBe('edit');
 
+            // Simulate use of Edit Button
+            await editButton.click();
+
+            const updateButton = await page.$('#updateButton');
+            await updateButton.click();
+            await navigationPromise;
 
             // Value of the Mood
+            await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/finish-page.html');
             const localMood = await page.evaluate(() => localStorage.getItem(key));
-            expect(localMood).toBe(`{\"rating\":\"${moodList[i]}\",\"comment\":\"\",\"edited\":false}`);
-            
-            // Proper Page Transfer
-            const titleFinish = await page.title();
-            expect(titleFinish).toBe('Finish Page');
+            expect(localMood).toBe(`{\"rating\":\"${moodList[i]}\",\"comment\":\"\",\"edited\":true}`);
 
-        }, 100000);
+        });
     }
 });
 
