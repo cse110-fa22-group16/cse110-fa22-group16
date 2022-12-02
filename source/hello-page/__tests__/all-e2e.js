5f1e5b8dd145e231f6fdc8985d376dd221b02072
hanghanghappy <chc063@ucsd.edu>
2022-12-01 22:41:19 -0800

    Updated all-e2e.js

--- source/hello-page/__tests__/all-e2e.js
+++ source/hello-page/__tests__/all-e2e.js
@@ -5,7 +5,7 @@ let year = dateObj.getFullYear();
 let key = `${year}-${month}-${date}`;
 const navigationPromise = page.waitForNavigation({ waitUntil: ['load', 'networkidle2'] });
 
-jest.setTimeout(500000);
+jest.setTimeout(200000);
 
 const moodList = ['terrible', 'bad', 'neutral', 'good', 'great'];
 
@@ -17,36 +17,57 @@ describe('Whole website test', () => {
     });    
     it('check let\'s go button link to prompt page, if not enter yet', async () => {
         await page.click('button');
-        await page.waitForNavigation();
         const title = await page.title();
         expect(title).toBe('Prompt Page');
     });
 
     // Prompt Page Test
-    for(let i=0; i<5; i++) {
-        it(`check finish button save the data to localStorage and link to finish page (${moodList[i]})`, async () => {
-            await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/prompt-page.html');            
+    beforeAll(async() => {
+        await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/prompt-page.html');
+    });
+    
+        for(let i=0; i<5; i++) {
+            it(`check finish button save the data to localStorage and link to finish page (${moodList[i]})`, async () => {
+                await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/prompt-page.html');
+    
+                const navigationPromise = page.waitForNavigation({ waitUntil: ['load', 'networkidle2'] });
+                
+                const btn= await page.$$('.feelingsChoices>input+label');
+                await btn[i].click();
+    
+                await page.click('button');
+                await navigationPromise;
+                const localMood = await page.evaluate(() => localStorage.getItem(key));
+    
+                expect(localMood).toBe(`{\"rating\":\"${moodList[i]}\",\"comment\":\"\",\"edited\":false}`);
+                
+                const title = await page.title();
+                expect(title).toBe('Finish Page');
+            }, 10000);
+        }
+});
+
+describe('Prompt Test and Edit Test', () => {
+    beforeAll(async() => {
+        await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/prompt-page.html');
+    });
+
+    for(let i = 0; i < 5; i++) {
+
+        it(`Check edit page for the following mood: (${moodList[i]})`, async () => {
+            await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/prompt-page.html');
+            const navigationPromise = page.waitForNavigation({ waitUntil: ['load', 'networkidle2'] });
             const btn= await page.$$('.feelingsChoices>input+label');
             await btn[i].click();
 
             await page.click('button');
             await navigationPromise;
-            const localMood = await page.evaluate(() => localStorage.getItem(key));
 
-            expect(localMood).toBe(`{\"rating\":\"${moodList[i]}\",\"comment\":\"\",\"edited\":false}`);
-            
-            const title = await page.title();
-            expect(title).toBe('Finish Page');
-        });
-    }
-
-    // Edit Test
-    for(let i = 0; i < 5; i++) {
-        it(`Check edit page for the following mood: (${moodList[i]})`, async () => {
-            await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/edit-page/edit-page.html');
-
+            // Title of Edit Page Check
+            await page.click('button');
             const titleEdit = await page.title();
             expect(titleEdit).toBe('Edit Page');
+
 
             // Text of the Edit Button
             const editButton = await page.$('#editButton');
@@ -59,16 +80,23 @@ describe('Whole website test', () => {
 
             const updateButton = await page.$('#updateButton');
             await updateButton.click();
+            await navigationPromise;
 
             // Value of the Mood
             await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/hello-page/finish-page.html');
-            await page.waitForNavigation();
             const localMood = await page.evaluate(() => localStorage.getItem(key));
             expect(localMood).toBe(`{\"rating\":\"${moodList[i]}\",\"comment\":\"\",\"edited\":true}`);
+
         });
     }
+});
 
-    // Delete Test
+// Delete Test
+describe('Delete Test', () => {
+    beforeAll(async() => {
+        await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/edit-page/edit-page.html');
+    });
+
     it(`Check delete test`, async () => {
         await page.goto('https://cse110-fa22-group16.github.io/cse110-fa22-group16/source/edit-page/edit-page.html');
 
@@ -89,7 +117,6 @@ describe('Whole website test', () => {
         await page.waitForNavigation();
         const title = await page.title();
         expect(title).toBe('Calendar Page');
-
     });
 });
 
